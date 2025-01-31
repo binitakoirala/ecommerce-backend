33787edca2f1eaa06e3d5b752abf47c59f1ae549
Binita Koirala <35364442+binitakoirala@users.noreply.github.com>
2025-01-31 15:41:14 +0000

    Create product add and delete API. (#11)

--- route/product_route.py
+++ route/product_route.py
@@ -8,3 +8,7 @@ product_bp = Blueprint("product_bp", __name__, url_prefix="/products")
 product_bp.route("/", methods=["GET"])(product_controller.get_products)
 
 product_bp.route("/<int:id>", methods=["GET"])(product_controller.get_product)
+
+product_bp.route("/", methods=["POST"])(product_controller.add_product)
+
+product_bp.route("/<int:id>", methods=["DELETE"])(product_controller.delete_product)
--- service/product_service.py
+++ service/product_service.py
@@ -1,4 +1,5 @@
 from ..model.product import Product
+from ..database import database
 
 
 class ProductService:
@@ -19,3 +20,40 @@ class ProductService:
         products: list[Product] = Product.query.all()
 
         return products
+
+    @classmethod
+    def add_product(cls, product: dict) -> Product:
+        """A function to add a new product."""
+        new_product = Product(
+            name=product.get("name"),
+            description=product.get("description"),
+            price=product.get("price"),
+            stock_quantity=product.get("stock_quantity"),
+            brand=product.get("brand"),
+            weight=product.get("weight"),
+            dimension=product.get("dimension"),
+            color=product.get("color"),
+        )
+
+        try:
+            database.session.add(new_product)
+            database.session.commit()
+            return new_product
+        except Exception as e:
+            database.session.rollback()
+            raise e
+
+    @classmethod
+    def delete_product_by_id(cls, id: int):
+        """A function to delete product by ID."""
+
+        product: Product = Product.query.get(id)
+
+        if product is None:
+            return None
+
+        Product.query.filter_by(id=id).delete()
+
+        database.session.commit()
+
+        return product
